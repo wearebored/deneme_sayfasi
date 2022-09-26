5e57cebd92c8fa174a195b770c082e370ae780bd
enes <enesooo78@gmail.com>
2022-09-26 22:19:44 +0300

    deneme

--- app.js
+++ app.js
@@ -17,35 +17,33 @@
 let val = document.querySelector("#select");
 let sayac = 0;
 val.disabled=true
-val.onclick = () => {
-  sayac += 1;
-  console.log(sayac);
-  if (sayac == "2") {
-    sayac = 0;
-    let sehir = val.value;
-    let url = `https://restcountries.com/v3.1/name/${sehir}`;
-    console.log(url);
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        const countryDiv = document.querySelector(".div1");
-        console.log(res[0]);
-        let {
-          capital,
-          currencies,
-          flags: { svg },
-          languages,
-          name: { common },
-          region,
-        } = res[0];
 
-        console.log(Object.values(languages));
-        console.log(Object.values(currencies)[0].name);
-        console.log(Object.values(currencies)[0].symbol);
-        console.log(countryDiv.children);
-        countryDiv.children[2].innerHTML = countryDiv.children[1].innerHTML;
-        countryDiv.children[1].innerHTML = countryDiv.children[0].innerHTML;
-        countryDiv.children[0].innerHTML = `
+val.onchange=()=>{
+  let sehir = val.value;
+  let url = `https://restcountries.com/v3.1/name/${sehir}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      const countryDiv = document.querySelector(".div1");
+      console.log(res[0]);
+      let {
+        capital,
+        currencies,
+        flags: { svg },
+        languages,
+        name: { common },
+        region,
+      } = res[0];
+
+      console.log(Object.values(languages));
+      console.log(Object.values(currencies)[0].name);
+      console.log(Object.values(currencies)[0].symbol);
+      console.log(countryDiv.children);
+
+      countryDiv.children[2].innerHTML = countryDiv.children[1].innerHTML;
+      countryDiv.children[1].innerHTML = countryDiv.children[0].innerHTML;
+      countryDiv.children[0].innerHTML = `
             <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
             <img src="${svg}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -76,7 +74,6 @@ val.onclick = () => {
 
 
         `;
-      });
-  }
-};
+    });
+}
 
